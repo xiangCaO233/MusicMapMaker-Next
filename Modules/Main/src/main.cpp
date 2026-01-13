fc0eb38ab64545b2bb1e5d0884275cde27ac6899
xiangCaO233 <odokoei@outlook.com>
2026-01-13 17:16:10 +0800

    测试vulkan实现

--- Modules/Main/src/main.cpp
+++ Modules/Main/src/main.cpp
@@ -1,9 +1,12 @@
 #include "colorful-log.h"
-
 #include "skin/SkinConfig.h"
 #include "translation/Translation.h"
+#include <array>
 #include <filesystem>
+#include <optional>
 #include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_handles.hpp>
+#include <vulkan/vulkan_structs.hpp>
 #define GLFW_INCLUDE_NONE
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -85,17 +88,21 @@ int main(int argc, char* argv[])
         XINFO("  - {}", ext);
     }
 
-    const std::vector<const char*> validationLayers = {
+    // 2.启用vk验证层
+    const std::array<const char*, 1> validationLayers = {
         "VK_LAYER_KHRONOS_validation"
     };
 
-    // 2.检查请求的验证层是否可用
+    // 2.1检查请求的验证层是否可用
     uint32_t layerCount;
     auto     res = vk::enumerateInstanceLayerProperties(&layerCount, nullptr);
+    // 先获取验证层的数量-会填入layerCount
     std::vector<vk::LayerProperties> availableLayers(layerCount);
     res = vk::enumerateInstanceLayerProperties(&layerCount,
                                                availableLayers.data());
+    // 然后才是获取所有的验证层-会填入availableLayers.data()
 
+    // 2.2检查请求到的验证层都能不能用
     bool allLayersAvailable = true;
     for ( const char* layerName : validationLayers ) {
         bool layerFound = false;
@@ -120,20 +127,83 @@ int main(int argc, char* argv[])
         XINFO("Validation layers enabled.");
     }
 
-    // 4. 填充应用程序信息 (vk::ApplicationInfo)
+    // 3. 应用程序信息 (vk::ApplicationInfo)
     vk::ApplicationInfo appInfo;
-    appInfo.sType              = vk::StructureType::eApplicationInfo;
-    appInfo.pApplicationName   = "MMM";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName        = "No Engine";
-    appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion         = VK_API_VERSION_1_2;  // 或者更高版本
+    appInfo.setPApplicationName("MMM")
+        .setApplicationVersion(VK_MAKE_VERSION(1, 0, 0))
+        .setApiVersion(VK_API_VERSION_1_4)
+        .setEngineVersion(VK_MAKE_VERSION(1, 0, 0))
+        .setPEngineName("No Engine");
 
-    // 创建vk实例
+    // 4.vk实例创建信息 (vk::InstanceCreateInfo)
     vk::InstanceCreateInfo instanceCreateInfo;
-    instanceCreateInfo.sType = vk::StructureType::eInstanceCreateInfo;
+    instanceCreateInfo.setPApplicationInfo(&appInfo).setPEnabledLayerNames(
+        validationLayers);
+
+    // 创建vk实例 (vk::Instance)
+    vk::Instance vkInstance = vk::createInstance(instanceCreateInfo);
+    XINFO("VK Instance created.");
+
+    // 4. 选择物理GPU设备
+    // 4.1. 列出所有物理GPU设备
+    std::vector<vk::PhysicalDevice> phyDevices =
+        vkInstance.enumeratePhysicalDevices();
+    for ( const auto& phyDevice : phyDevices ) {
+        // 4.1 可以在这里检查GPU设备的功能并选择
+        auto devFeature = phyDevice.getFeatures();
+    }
+    // 默认选择第一个GPU设备
+    vk::PhysicalDevice selectedPhyDevice = phyDevices[0];
+    XINFO("Selected GPU:\n-- {}",
+          std::string(selectedPhyDevice.getProperties().deviceName));
+
+    // 5. vk逻辑设备创建信息
+    vk::DeviceCreateInfo vkDeviceCreateInfo;
+    // 6. vk逻辑设备队列创建信息
+    vk::DeviceQueueCreateInfo vkDeviceQueueCreateInfo;
+    float                     priorities{ 1.f };  // 优先级表
+    struct QueueFamilyIndicies final {
+        std::optional<uint32_t> graphicsQueue;
+    };
+    QueueFamilyIndicies queueFamilyIndicies;
+    // 6.1 查询QueueFamilyIndicies
+    auto phyDeviceQueueFamilyProperties =
+        selectedPhyDevice.getQueueFamilyProperties();
+    for ( size_t i{ 0 }; i < phyDeviceQueueFamilyProperties.size(); ++i ) {
+        const auto& phyDeviceQueueFamilyProperty =
+            phyDeviceQueueFamilyProperties[i];
+        auto& queueFlags = phyDeviceQueueFamilyProperty.queueFlags;
+        if ( queueFlags | vk::QueueFlagBits::eGraphics ) {
+            // 得是图形显卡
+            queueFamilyIndicies.graphicsQueue = i;
+            break;
+        }
+    }
+    vkDeviceQueueCreateInfo.setPQueuePriorities(&priorities)
+        .setQueueCount(1)
+        .setQueueFamilyIndex(queueFamilyIndicies.graphicsQueue.value());
+
+    // 将队列创建信息设置到逻辑设备创建信息中
+    vkDeviceCreateInfo.setQueueCreateInfos(vkDeviceQueueCreateInfo);
+    // 6.2创建vk逻辑设备 (通过物理设备)
+    vk::Device vkDevice = selectedPhyDevice.createDevice(vkDeviceCreateInfo);
+
+    // 6.3获取逻辑设备队列
+    vk::Queue graphicsQueue =
+        vkDevice.getQueue(queueFamilyIndicies.graphicsQueue.value(), 0);
+
+    //
+
+
+    // 在销毁实例之前，先销毁逻辑设备
+    vkDevice.destroy();
+    XINFO("VK Device destroyed.");
+    // 销毁vk实例
+    vkInstance.destroy();
+    XINFO("VK Instance destroyed.");
 
     glfwTerminate();
+    XINFO("GLFW Terminated.");
     XLogger::shutdown();
     return 0;
 }
