a9299275a1eee7a951a8ffddef25e0a7290a829a
xiangCaO233 <odokoei@outlook.com>
2026-01-15 01:12:15 +0800

    测试Vulkan实现

--- Modules/Main/src/main.cpp
+++ Modules/Main/src/main.cpp
@@ -1,6 +1,7 @@
 #include "colorful-log.h"
 #include "skin/SkinConfig.h"
 #include "translation/Translation.h"
+#include "vulkan/vulkan.hpp"
 #include <array>
 #include <filesystem>
 #include <optional>
@@ -10,6 +11,12 @@
 #define GLFW_INCLUDE_NONE
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+// vk debug回调
+VKAPI_ATTR VkBool32 VKAPI_PTR vkDebug_callback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT      messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT             messageTypes,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
 
 int main(int argc, char* argv[])
 {
@@ -62,7 +69,10 @@ int main(int argc, char* argv[])
     };
     XINFO("GLFW initialized successfully.");
 
-    // 1.1检查 GLFW 的 Vulkan 是否被支持
+    // 1.1设置GLFW为NOAPI来适配vulkan
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    // 1.2检查 GLFW 的 Vulkan 是否被支持
     if ( !glfwVulkanSupported() ) {
         XERROR("Fatal: GLFW reports that Vulkan is not supported!");
         glfwTerminate();
@@ -70,9 +80,9 @@ int main(int argc, char* argv[])
     }
     XINFO("GLFW Vulkan is supported.");
 
-    // 1.2获取 GLFW 需要的扩展
+    // 1.3获取 GLFW 需要的扩展
     uint32_t     glfwExtensionCount = 0;
-    const char** glfwExtensions;
+    const char** glfwExtensions{ nullptr };
     glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
     if ( glfwExtensions == nullptr ) {
         XERROR("Fatal: Failed to get required GLFW extensions.");
@@ -80,17 +90,30 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    // 1.3将 C 风格的字符串数组转换为更易于管理的 std::vector
+    // 1.4将 C 风格的字符串数组转换为更易于管理的 std::vector
     std::vector<const char*> requiredExtensions(
         glfwExtensions, glfwExtensions + glfwExtensionCount);
     XINFO("Required GLFW extensions:");
     for ( const auto& ext : requiredExtensions ) {
         XINFO("  - {}", ext);
     }
+    // 启用vk的debug工具扩展
+    requiredExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+    // 1.5创建GLFW窗口
+    GLFWwindow* glfwWindow =
+        glfwCreateWindow(1920, 1080, "MMMTestVulkan", nullptr, nullptr);
+
+    if ( !glfwWindow ) {
+        XERROR("glfw窗口创建失败");
+        glfwTerminate();
+        return -1;
+    }
+
 
     // 2.启用vk验证层
     const std::array<const char*, 1> validationLayers = {
-        "VK_LAYER_KHRONOS_validation"
+        "VK_LAYER_KHRONOS_validation",
     };
 
     // 2.1检查请求的验证层是否可用
@@ -127,73 +150,166 @@ int main(int argc, char* argv[])
         XINFO("Validation layers enabled.");
     }
 
-    // 3. 应用程序信息 (vk::ApplicationInfo)
-    vk::ApplicationInfo appInfo;
+    // 3.vk调试创建信息 (vk::DebugUtilsMessengerCreateInfoEXT)
+    vk::DebugUtilsMessengerCreateInfoEXT vkDebugUtilcreateInfo{};
+    vkDebugUtilcreateInfo
+        // 设置严重等级
+        .setMessageSeverity(
+            // 接收错误
+            vk::DebugUtilsMessageSeverityFlagBitsEXT::eError |
+            // 接收警告
+            vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning
+            // 接收信息
+            // | vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo
+            // 接收详细调试信息（非常吵，建议开发时开启）
+            // | vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose
+            )
+        // 设置消息类型
+        .setMessageType(
+            // 常规类型
+            vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+            // 验证层类型
+            vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation |
+            // 性能类型
+            vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance)
+        // 设置回调函数指针
+        .setPfnUserCallback(
+            vk::PFN_DebugUtilsMessengerCallbackEXT(vkDebug_callback));
+
+
+    // 4. 应用程序信息 (vk::ApplicationInfo)
+    vk::ApplicationInfo appInfo{};
     appInfo.setPApplicationName("MMM")
         .setApplicationVersion(VK_MAKE_VERSION(1, 0, 0))
         .setApiVersion(VK_API_VERSION_1_4)
         .setEngineVersion(VK_MAKE_VERSION(1, 0, 0))
         .setPEngineName("No Engine");
 
-    // 4.vk实例创建信息 (vk::InstanceCreateInfo)
-    vk::InstanceCreateInfo instanceCreateInfo;
-    instanceCreateInfo.setPApplicationInfo(&appInfo).setPEnabledLayerNames(
-        validationLayers);
+    // 5.vk实例创建信息 (vk::InstanceCreateInfo)
+    vk::InstanceCreateInfo instanceCreateInfo{};
+    instanceCreateInfo.setPApplicationInfo(&appInfo)
+        .setPEnabledLayerNames(validationLayers)
+        .setPEnabledExtensionNames(requiredExtensions)
+        .setPNext(&vkDebugUtilcreateInfo);
 
     // 创建vk实例 (vk::Instance)
     vk::Instance vkInstance = vk::createInstance(instanceCreateInfo);
     XINFO("VK Instance created.");
 
-    // 4. 选择物理GPU设备
-    // 4.1. 列出所有物理GPU设备
+    // 5.1初始化vkDebugMessenger
+    // 在全局或类成员中定义
+    vk::DebugUtilsMessengerEXT        vkDebugMessenger;
+    vk::detail::DispatchLoaderDynamic dldy;  // 动态加载器
+    // 5.2. 初始化动态加载器（它会去寻找 vkCreateDebugUtilsMessengerEXT 的地址）
+    dldy.init(vkInstance, vkGetInstanceProcAddr);
+    // 5.3. 创建 Messenger 对象
+    // 这里必须传入 dldy 否则会链接报错
+    vkDebugMessenger = vkInstance.createDebugUtilsMessengerEXT(
+        vkDebugUtilcreateInfo, nullptr, dldy);
+    XINFO("Vulkan Debug Messenger 开启成功");
+
+
+    // 6. 选择GPU设备
+    // 6.1. 列出所有物理GPU设备
     std::vector<vk::PhysicalDevice> phyDevices =
         vkInstance.enumeratePhysicalDevices();
     for ( const auto& phyDevice : phyDevices ) {
         // 4.1 可以在这里检查GPU设备的功能并选择
-        auto devFeature = phyDevice.getFeatures();
+        auto devFeature    = phyDevice.getFeatures();
+        auto devProperties = phyDevice.getProperties();
+        auto devFeatures   = phyDevice.getFeatures();
+        // 如优先选择独立显卡
+        // devProperties.deviceType == vk::PhysicalDeviceType::eDiscreteGpu;
     }
-    // 默认选择第一个GPU设备
+    // 简单起见先选择第一个GPU设备
     vk::PhysicalDevice selectedPhyDevice = phyDevices[0];
+
     XINFO("Selected GPU:\n-- {}",
           std::string(selectedPhyDevice.getProperties().deviceName));
 
-    // 5. vk逻辑设备创建信息
-    vk::DeviceCreateInfo vkDeviceCreateInfo;
-    // 6. vk逻辑设备队列创建信息
-    vk::DeviceQueueCreateInfo vkDeviceQueueCreateInfo;
+    // 6.2 vk逻辑设备创建信息
+    vk::DeviceCreateInfo vkDeviceCreateInfo{};
+
+    // 6.3 vk逻辑设备队列创建信息
+    vk::DeviceQueueCreateInfo vkDeviceQueueCreateInfo{};
     float                     priorities{ 1.f };  // 优先级表
-    struct QueueFamilyIndicies final {
+
+    // 6.4 查询图形队列族索引
+    struct QueueFamilyIndex final {
         std::optional<uint32_t> graphicsQueue;
     };
-    QueueFamilyIndicies queueFamilyIndicies;
-    // 6.1 查询QueueFamilyIndicies
+    QueueFamilyIndex queueFamilyIndex{};
+
     auto phyDeviceQueueFamilyProperties =
         selectedPhyDevice.getQueueFamilyProperties();
     for ( size_t i{ 0 }; i < phyDeviceQueueFamilyProperties.size(); ++i ) {
         const auto& phyDeviceQueueFamilyProperty =
             phyDeviceQueueFamilyProperties[i];
-        auto& queueFlags = phyDeviceQueueFamilyProperty.queueFlags;
-        if ( queueFlags | vk::QueueFlagBits::eGraphics ) {
-            // 得是图形显卡
-            queueFamilyIndicies.graphicsQueue = i;
+        const auto& queueFlags = phyDeviceQueueFamilyProperty.queueFlags;
+
+        if ( queueFlags & vk::QueueFlagBits::eGraphics ) {
+            // 使用图形功能
+            queueFamilyIndex.graphicsQueue = i;
             break;
         }
     }
-    vkDeviceQueueCreateInfo.setPQueuePriorities(&priorities)
-        .setQueueCount(1)
-        .setQueueFamilyIndex(queueFamilyIndicies.graphicsQueue.value());
+    if ( !queueFamilyIndex.graphicsQueue.has_value() ) {
+        XERROR("Graphic Queue Family not found");
+        // 销毁调试消息器 (在 Instance 销毁之前)
+        // 需要传入之前初始化的 dldy
+        if ( vkDebugMessenger ) {
+            vkInstance.destroyDebugUtilsMessengerEXT(
+                vkDebugMessenger, nullptr, dldy);
+            XINFO("VK Debug Messenger destroyed.");
+        }
 
-    // 将队列创建信息设置到逻辑设备创建信息中
-    vkDeviceCreateInfo.setQueueCreateInfos(vkDeviceQueueCreateInfo);
-    // 6.2创建vk逻辑设备 (通过物理设备)
+        // 销毁vk实例
+        vkInstance.destroy();
+        XINFO("VK Instance destroyed.");
+
+        glfwTerminate();
+        XINFO("GLFW Terminated.");
+        return -1;
+    }
+
+    vkDeviceQueueCreateInfo
+        // 优先级表
+        .setPQueuePriorities(&priorities)
+        .setQueueCount(1)
+        .setQueueFamilyIndex(queueFamilyIndex.graphicsQueue.value());
+
+    // 6.5将队列创建信息设置到逻辑设备创建信息中
+    vkDeviceCreateInfo
+        .setQueueCreateInfos(vkDeviceQueueCreateInfo)
+        // 启用逻辑设备功能
+        .setPEnabledFeatures({})
+        // 启用逻辑设备扩展
+        .setPEnabledExtensionNames({});
+
+    // 6.6创建vk逻辑设备 (通过物理设备)
     vk::Device vkDevice = selectedPhyDevice.createDevice(vkDeviceCreateInfo);
 
-    // 6.3获取逻辑设备队列
+
+    // 6.7获取逻辑设备队列
     vk::Queue graphicsQueue =
-        vkDevice.getQueue(queueFamilyIndicies.graphicsQueue.value(), 0);
+        vkDevice.getQueue(queueFamilyIndex.graphicsQueue.value(), 0);
 
     //
 
+    // 进入glfw窗口循环
+    while ( !glfwWindowShouldClose(glfwWindow) ) {
+        // 渲染循环
+        glfwPollEvents();
+        glfwSwapBuffers(glfwWindow);
+    }
+
+    // 销毁调试消息器 (在 Instance 销毁之前)
+    // 需要传入之前初始化的 dldy
+    if ( vkDebugMessenger ) {
+        vkInstance.destroyDebugUtilsMessengerEXT(
+            vkDebugMessenger, nullptr, dldy);
+        XINFO("VK Debug Messenger destroyed.");
+    }
 
     // 在销毁实例之前，先销毁逻辑设备
     vkDevice.destroy();
@@ -206,4 +322,40 @@ int main(int argc, char* argv[])
     XINFO("GLFW Terminated.");
     XLogger::shutdown();
     return 0;
+}
+
+// vk debug回调
+VKAPI_ATTR VkBool32 VKAPI_CALL vkDebug_callback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT      messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT             messageTypes,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData)
+{
+    // 1. 整理消息前缀（显示消息类型：常规/校验/性能）
+    std::string typeHeader;
+    if ( messageTypes & VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT )
+        typeHeader = "[General]";
+    else if ( messageTypes & VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT )
+        typeHeader = "[Validation]";
+    else if ( messageTypes & VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT )
+        typeHeader = "[Performance]";
+
+    // 2. 根据 Severity 等级分发到不同的日志宏
+    if ( messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT ) {
+        XERROR("{} {}", typeHeader, pCallbackData->pMessage);
+    } else if ( messageSeverity &
+                VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT ) {
+        XWARN("{} {}", typeHeader, pCallbackData->pMessage);
+    } else if ( messageSeverity &
+                VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT ) {
+        XINFO("{} {}", typeHeader, pCallbackData->pMessage);
+    } else if ( messageSeverity &
+                VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT ) {
+        // 对应 Debug 或 Trace 级别
+        XDEBUG("{} {}", typeHeader, pCallbackData->pMessage);
+    }
+
+    // 返回 VK_FALSE 表示不中断触发该消息的 Vulkan 调用
+    // 如果返回 VK_TRUE，则该 API 调用会返回 VK_ERROR_VALIDATION_FAILED_EXT
+    // 并可能中断程序
+    return VK_FALSE;
 }
