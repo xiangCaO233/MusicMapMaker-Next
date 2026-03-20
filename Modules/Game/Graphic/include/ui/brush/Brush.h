b45f36a8bb4268a43c60128c74c35650e2ec640b
xiangCaO233 <odokoei@outlook.com>
2026-03-21 03:49:01 +0800

    feat: 定义画笔

--- Modules/Game/Graphic/include/ui/brush/Brush.h
+++ Modules/Game/Graphic/include/ui/brush/Brush.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "BrushDrawCmd.h"
+#include "graphic/imguivk/mesh/VKVertex.h"
+#include <glm/fwd.hpp>
+
 namespace MMM::Graphic::UI
 {
 
@@ -13,6 +17,29 @@ public:
     Brush& operator=(const Brush&) = default;
     ~Brush()                       = default;
 
+    /// @brief 清理顶点缓存
+    void clear();
+
+    /// @brief 绘制一个矩形
+    void drawRect(glm::vec2 min, glm::vec2 max, uint32_t color,
+                  float thickness = 1.0f);
+
+    // --- 获取数据供 Vulkan 使用 ---
+    inline const std::vector<VKVertex>& getVertices() const
+    {
+        return m_vertices;
+    }
+    inline const std::vector<uint32_t>& getIndices() const { return m_indices; }
+    inline const std::vector<DrawCmd>&  getCmds() const { return m_cmds; }
+
 private:
+    /// @brief 顶点缓存
+    std::vector<VKVertex> m_vertices;
+
+    /// @brief 顶点索引缓存
+    std::vector<uint32_t> m_indices;
+
+    /// @brief 绘制指令缓存
+    std::vector<DrawCmd> m_cmds;
 };
 }  // namespace MMM::Graphic::UI
--- Modules/Game/Graphic/src/ui/brush/Brush.cpp
+++ Modules/Game/Graphic/src/ui/brush/Brush.cpp
@@ -1,1 +1,21 @@
 #include "ui/brush/Brush.h"
+#include <glm/glm.hpp>
+
+
+namespace MMM::Graphic::UI
+{
+
+/// @brief 清理顶点缓存
+void Brush::clear()
+{
+    m_vertices.clear();
+    m_indices.clear();
+}
+
+/// @brief 绘制一个矩形
+void Brush::drawRect(glm::vec2 min, glm::vec2 max, uint32_t color,
+                     float thickness)
+{
+}
+
+}  // namespace MMM::Graphic::UI
