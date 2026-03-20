49f44c68961b0f0931082ac0c9fb9c5c43400d41
xiangCaO233 <odokoei@outlook.com>
2026-03-21 03:46:03 +0800

    chore: 添加光标资源

--- assets/skins/mmm-nightly/skin.lua
+++ assets/skins/mmm-nightly/skin.lua
@@ -17,14 +17,16 @@ local Skin = {
 		alert = { 1.0, 0.2, 0.2, 1.0 },
 	},
 
-    fonts = {
-        ascii = "fonts/ComicShannsMonoNerdFontPropo-Regular.otf",
-        cjk = "fonts/NotoSansMonoCJKsc-Regular.otf",
-    },
+	fonts = {
+		ascii = "fonts/ComicShannsMonoNerdFontPropo-Regular.otf",
+		cjk = "fonts/NotoSansMonoCJKsc-Regular.otf",
+	},
 
 	-- 资产文件映射
 	assets = {
 		cursor = "images/cursor/cursor.png",
+		cursortrail = "images/cursor/cursortrail.png",
+		cursor_smoke = "images/cursor/cursor-smoke.png",
 		btn_play = "images/buttons/play.png",
 		btn_pause = "images/buttons/pause.png",
 		bg_main = "images/backgrounds/main_menu.jpg",
