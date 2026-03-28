358a1f3745a6b79c4cc2283c4938f23c21844c9a
xiangCaO233 <odokoei@outlook.com>
2026-03-28 10:22:16 +0800

    chore: 修复msvc构建问题

--- 3rdpty/sources/cmake/lunasvg.cmake
+++ 3rdpty/sources/cmake/lunasvg.cmake
@@ -2,8 +2,9 @@
 set(LUNASVG_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lunasvg SYSTEM)
-
+set_source_files_properties("${CMAKE_CURRENT_SOURCE_DIR}/lunasvg/plutovg/source/plutovg-font.c"
+	PROPERTIES COMPILE_FLAGS "/Od"
+)
 # 创建接口库
 add_library(3rd_lunasvg INTERFACE)
 target_link_libraries(3rd_lunasvg INTERFACE lunasvg::lunasvg)
-
