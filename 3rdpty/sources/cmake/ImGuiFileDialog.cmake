e2c050de391f51519f3eebcf2ce990d9c3711559
bbk9961 <160425538+bbk9961@users.noreply.github.com>
2026-04-27 21:00:23 +0100

    fix: UTF-8 charset
    修复windows环境下编码解析错误

--- 3rdpty/sources/cmake/ImGuiFileDialog.cmake
+++ 3rdpty/sources/cmake/ImGuiFileDialog.cmake
@@ -1,6 +1,11 @@
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/ImGuiFileDialog SYSTEM)
 target_link_libraries(ImGuiFileDialog PUBLIC 3rd_imgui)
 
+# 强制 Clang 将字符集设为 UTF-8,修复windows环境下传递宏定义时的编码解析错误 
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    target_compile_options(ImGuiFileDialog PRIVATE "-finput-charset=UTF-8" "-fexec-charset=UTF-8")
+endif()
+
 # 国际化翻译 (由于该库使用宏定义 UI 文本，我们通过编译定义进行注入)
 target_compile_definitions(ImGuiFileDialog PRIVATE
     "okButtonString=\"确定\""
