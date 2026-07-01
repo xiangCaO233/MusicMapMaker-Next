06c6de2fee2e9fabf09046ba948e2f344419028c
xiangCaO233 <odokoei@outlook.com>
2026-07-01 14:48:20 +0800

    chore: 更新mingw-gcc预编译库版本

--- scripts/ci/cross/mingw-gcc-build.sh
+++ scripts/ci/cross/mingw-gcc-build.sh
@@ -16,6 +16,8 @@ Options:
   --prefix <prefix>       MinGW tool prefix. Default: x86_64-w64-mingw32
   --sysroot <path>        MinGW sysroot. Default: <prefix>-gcc -print-sysroot, then /usr/x86_64-w64-mingw32
   --toolchain <path>      CMake toolchain file. Default: cmake/toolchain/cross-mingw-gcc.cmake
+  --sources-build         Configure with SOURCES_BUILD=ON.
+  --prebuilt-targets      Build only third-party targets used for staging.
   --configure-only        Configure and generate, then stop
   --fresh                 Remove the build directory before configuring
   -h, --help              Show this help
@@ -102,6 +104,8 @@ projectLinkage="static"
 toolPrefix="x86_64-w64-mingw32"
 mingwSysroot=""
 toolchainFile="cmake/toolchain/cross-mingw-gcc.cmake"
+sourcesBuild="OFF"
+prebuiltTargets=0
 configureOnly=0
 freshBuild=0
 
@@ -171,6 +175,14 @@ while (( $# > 0 )); do
             toolchainFile="$2"
             shift 2
             ;;
+        --sources-build)
+            sourcesBuild="ON"
+            shift
+            ;;
+        --prebuilt-targets)
+            prebuiltTargets=1
+            shift
+            ;;
         --configure-only)
             configureOnly=1
             shift
@@ -228,6 +240,10 @@ requireCommand cmake
 requireCommand "${toolPrefix}-gcc"
 requireCommand "${toolPrefix}-g++"
 requireCommand "${toolPrefix}-windres"
+requireCommand "${toolPrefix}-ar"
+requireCommand "${toolPrefix}-ranlib"
+requireCommand "${toolPrefix}-strip"
+requireCommand "${toolPrefix}-objcopy"
 
 if [[ ! -d "${MINGW_SYSROOT}" ]]; then
     printf "error: MINGW_SYSROOT does not exist: %s\n" "${MINGW_SYSROOT}" >&2
@@ -252,7 +268,9 @@ cmake -G "${CMAKE_GENERATOR:-Ninja}" \
     -DCMAKE_TOOLCHAIN_FILE="${toolchainFile}" \
     -DMINGW_SYSROOT="${MINGW_SYSROOT}" \
     -DMINGW_TOOLCHAIN_PREFIX="${toolPrefix}" \
+    -DSOURCES_BUILD="${sourcesBuild}" \
     -DPROJECT_LINKAGE="${projectLinkage}" \
+    -DICE_LINKAGE="${projectLinkage}" \
     -DPROJECT_PREBUILT_COMPILER_TAG="${compilerTag}" \
     -DMMM_PGO_INSTRUMENT=OFF \
     -DMMM_PGO_USE=OFF \
@@ -263,4 +281,30 @@ if (( configureOnly )); then
     exit 0
 fi
 
-cmake --build "${buildDir}" --parallel "${buildJobs}"
+if (( prebuiltTargets )); then
+    cmake --build "${buildDir}" --parallel "${buildJobs}" --target \
+        zlib_project \
+        lame_project \
+        ffmpeg_project \
+        fftw_project \
+        rubberband_project \
+        samplerate \
+        IonCachyEngine-static \
+        3rd_implot \
+        3rd_miniz \
+        imgui-static \
+        freetype \
+        glfw \
+        ImGuiFileDialog \
+        nfd \
+        lunasvg \
+        plutovg \
+        libcurl_static \
+        fmt \
+        spdlog \
+        OpenAL \
+        SDL3-static \
+        luajit_build
+else
+    cmake --build "${buildDir}" --parallel "${buildJobs}"
+fi
